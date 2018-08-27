3a1b56f4cf2cd26134eda30fcde884275f493559
tock203 <tick@tock.tech>
2018-08-28 02:28:41 +0900

    bugfix: It won't upload test result if test fails

--- run.sh
+++ run.sh
@@ -4,12 +4,12 @@ test -d lightning-integration \
   || git clone https://github.com/cdecker/lightning-integration.git lightning-integration \
   || exit 1
 
-set -e
 cd lightning-integration
+git pull || exit 1
 
-git pull
+mkdir output
 
-docker build --no-cache -t lightning-integration .
+docker build --no-cache -t lightning-integration . || exit 1
 docker run \
   -v reports:/root/lightning-integration/reports \
   -v output:/root/lightning-integration/output \
