c75afd762688ff519b3c6a55149cb806bf1adf12
tock203 <tick@tock.tech>
2018-08-28 02:28:41 +0900

    bugfix: It won't upload test result if test fails

--- run.sh
+++ run.sh
@@ -4,15 +4,13 @@ test -d lightning-integration \
   || git clone https://github.com/cdecker/lightning-integration.git lightning-integration \
   || exit 1
 
-set -e
 cd lightning-integration
+git pull || exit 1
 
-git pull
-
-docker build --no-cache -t lightning-integration .
+docker build --no-cache -t lightning-integration . || exit 1
 docker run \
-  -v reports:/root/lightning-integration/reports \
-  -v output:/root/lightning-integration/output \
+  -v $(pwd)/reports:/root/lightning-integration/reports \
+  -v $(pwd)/output:/root/lightning-integration/output \
   lightning-integration make update clients test site
 
 az storage blob upload-batch --account-name $AZURE_STORAGE_ACCOUNT -d '$web/reports' -s reports
