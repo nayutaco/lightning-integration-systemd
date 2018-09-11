766e5e3ebdb61a5f6b360b1464aca71458e83e0b
tock203 <tick@tock.tech>
2018-09-11 18:52:15 +0900

    Remove image and container after job get done

--- run.sh
+++ run.sh
@@ -9,9 +9,11 @@ git pull || exit 1
 
 docker build --no-cache -t lightning-integration . || exit 1
 docker run \
+  --rm \
   -v $(pwd)/reports:/root/lightning-integration/reports \
   -v $(pwd)/output:/root/lightning-integration/output \
   lightning-integration make update clients test site
 
+docker rmi lightning-integration
 az storage blob upload-batch --account-name $AZURE_STORAGE_ACCOUNT -d '$web/reports' -s reports
 az storage blob upload-batch --account-name $AZURE_STORAGE_ACCOUNT -d '$web/output' -s output
